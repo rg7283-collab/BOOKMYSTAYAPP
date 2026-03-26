36d540ed8052e5be12332d008e73bc70d2d2bd7c
rg7283-collab <rg728@srmist.edu.in>
2026-03-26 11:30:10 +0530

    UC2:Basic Room Types & Static Availability

--- src/BOOKMYSTAYAPP.java
+++ src/BOOKMYSTAYAPP.java
@@ -1,5 +1,100 @@
-public class BOOKMYSTAYAPP {
-    static void main(){
-        System.out.println("Welcome To The Hotel Booking Management System\nSystem Initilize Successfully");
+abstract class Room {
+    private String roomType;
+    private int beds;
+    private int size;
+    private double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void displayDetails(int availability);
+}
+
+// Single Room
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 250, 1500.0);
+    }
+
+    @Override
+    public void displayDetails(int availability) {
+        System.out.println("Single Room:");
+        System.out.println("Beds: " + getBeds());
+        System.out.println("Size: " + getSize() + " sqft");
+        System.out.println("Price per night: " + getPrice());
+        System.out.println("Available: " + availability + "\n");
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 400, 2500.0);
+    }
+
+    @Override
+    public void displayDetails(int availability) {
+        System.out.println("Double Room:");
+        System.out.println("Beds: " + getBeds());
+        System.out.println("Size: " + getSize() + " sqft");
+        System.out.println("Price per night: " + getPrice());
+        System.out.println("Available: " + availability + "\n");
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 750, 5000.0);
+    }
+
+    @Override
+    public void displayDetails(int availability) {
+        System.out.println("Suite Room:");
+        System.out.println("Beds: " + getBeds());
+        System.out.println("Size: " + getSize() + " sqft");
+        System.out.println("Price per night: " + getPrice());
+        System.out.println("Available: " + availability + "\n");
+    }
+}
+
+// Main Class
+class UseCase2RoomInitialization {
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization\n");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Static availability
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        single.displayDetails(singleAvailability);
+        doubleRoom.displayDetails(doubleAvailability);
+        suite.displayDetails(suiteAvailability);
     }
 }
